cf496835a640af24cca26e5b1cad4436b5049e29
piffy <piffy@silencio>
2013-04-16 22:28:12 +0200

    Baseline

---
+++ El_Merendero/src/el_merendero/El_Merendero.java
@@ -0,0 +1,18 @@
+package el_merendero;
+
+/**
+ *
+ * @author piffy
+ */
+public class El_Merendero {
+    /**
+     * (tiene traccia delle versioni)
+     */
+    public static final float VERSION=0.01f; 
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("El Merendero versione "+VERSION);
+    }
+}
