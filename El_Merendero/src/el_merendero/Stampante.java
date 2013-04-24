e6afa4249167eed8d4a1341f89b406756d04d1bd
Marcello Missiroli <prof.missiroli@gmail.com>
2013-04-24 12:15:12 +0200

    Fine prototipo Stampa (EDDY, MAN, BIGL)

--- El_Merendero/src/el_merendero/Stampante.java
+++ El_Merendero/src/el_merendero/Stampante.java
@@ -24,12 +24,14 @@ import javax.swing.JOptionPane;
  */
 public class Stampante implements Printable {
 
-    private final int MARGINE = 100;
+    private final int MARGINE = 150;
     private final int MARGINE_DESTRO = 500;
     private final int SPAZIATURA = 15;
     private LinkedList<String> merende;
+    private LinkedList<Integer> numeri;
     private LinkedList<Float> prezzi;
-    private Image image;
+    private Image image1;
+    private Image image2;
     private String classe;
     private String aula;
     private Calendar data;
@@ -40,10 +42,11 @@ public class Stampante implements Printable {
      */
     public Stampante() {
         merende = new LinkedList<String>();
+        numeri = new LinkedList<Integer>();
         prezzi = new LinkedList<Float>();
-        image = new ImageIcon(this.getClass().getResource("Logo.jpg")).getImage();
+        image1 = new ImageIcon(this.getClass().getResource("Logo.jpg")).getImage();
     }
-    
+
     /**
      * Metodo che genera il layout della pagina da stampare
      * @param g Layout della pagina
@@ -57,32 +60,39 @@ public class Stampante implements Printable {
             return NO_SUCH_PAGE;
         }
 
-        g.drawImage(image, 5, 5, null);
-        
+        g.drawImage(image1, 50, 20, null);
+        image2 = new ImageIcon(this.getClass().getResource("Logo2.jpg")).getImage();
+        g.drawImage(image2, 215, 20, null);
+
         FontMetrics fm = g.getFontMetrics();
         String appoggio;
         appoggio = "Classe " + classe;
-        g.drawString(appoggio, MARGINE_DESTRO - fm.stringWidth(classe), 30);
-        appoggio = data.get(Calendar.HOUR_OF_DAY) + ":" + data.get(Calendar.MINUTE) + "   " + data.get(Calendar.DATE) + "/" + data.get(Calendar.MONTH) + "/" + data.get(Calendar.YEAR);
-        g.drawString(appoggio, MARGINE_DESTRO - fm.stringWidth(appoggio), 45);
+        g.drawString(appoggio, MARGINE_DESTRO - fm.stringWidth(appoggio), 50);
+        appoggio = data.get(Calendar.HOUR_OF_DAY) + ":" + data.get(Calendar.MINUTE) + " " + data.get(Calendar.DATE) + "/" + data.get(Calendar.MONTH) + "/" + data.get(Calendar.YEAR);
+        g.drawString(appoggio, MARGINE_DESTRO - fm.stringWidth(appoggio), 65);
         appoggio = "Aula " + aula;
-        g.drawString(appoggio, MARGINE_DESTRO - fm.stringWidth(appoggio), 60);
+        g.drawString(appoggio, MARGINE_DESTRO - fm.stringWidth(appoggio), 80);
 
         float totale = 0;
         int i;
         for (i = 0; i < merende.size(); i++) {
-            appoggio = merende.get(i);
-            g.drawString(appoggio, MARGINE, 120 + SPAZIATURA * i);
-            appoggio = "" + prezzi.get(i);
+            appoggio = prezzi.get(i) + " €";
             g.drawString(appoggio, MARGINE_DESTRO - fm.stringWidth(appoggio), 120 + SPAZIATURA * i);
+            int lungi = fm.stringWidth(appoggio);
+            appoggio =  numeri.get(i)+" - "+merende.get(i);
+            while ( MARGINE_DESTRO - MARGINE - fm.stringWidth(appoggio) - lungi > 10) {
+                appoggio+=".";
+            }
+            g.drawString(appoggio, MARGINE, 120 + SPAZIATURA * i);
             totale += prezzi.get(i);
         }
-
-        g.setFont(new Font(Font.DIALOG, Font.BOLD, Font.PLAIN));
+        g.setFont(new Font("suns", Font.BOLD, 12));
         appoggio = "Totale: " + totale + " €";
         g.drawString(appoggio, MARGINE_DESTRO - fm.stringWidth(appoggio), 120 + SPAZIATURA * i);
-        g.drawString("Soldi forniti: " + soldiForniti + " €", MARGINE + 50, 150 + SPAZIATURA * i);
-        g.drawString("Resto: " + (soldiForniti - totale) + " €", MARGINE + 250, 150 + SPAZIATURA * i);
+        appoggio = "Soldi forniti: " + soldiForniti + " €";
+        g.drawString(appoggio, MARGINE_DESTRO - fm.stringWidth(appoggio) - 5, 140 + SPAZIATURA * i);
+        appoggio = "Resto: " + (soldiForniti - totale) + " €";
+        g.drawString(appoggio, MARGINE_DESTRO - fm.stringWidth(appoggio), 160 + SPAZIATURA * i);
 
         return PAGE_EXISTS;
     }
@@ -92,8 +102,9 @@ public class Stampante implements Printable {
      * @param merenda Nome della merenda
      * @param prezzo Prezzo della merenda
      */
-    public void add(String merenda, float prezzo) {
+    public void add(String merenda,int numero, float prezzo) {
         merende.add(merenda);
+        numeri.add(numero);
         prezzi.add(prezzo);
     }
 
@@ -181,10 +192,10 @@ public class Stampante implements Printable {
         st.addClasse("4B Info");
         st.addAula("216");
         st.addSoldiForniti(17.5f);
-        st.add("Panino", 5.6f);
-        st.add("Panino", 1.1f);
-        st.add("Panino", 3.9f);
-        st.add("Panino", 2.4f);
+        st.add("Cotoletta",2, 5.80f);
+        st.add("Hot-Dog",1, 1.20f);
+        st.add("Forno",3, 3.00f);
+        st.add("Piadina",4, 2.50f);
         st.print();
     }
 }
