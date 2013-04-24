075f747830e5ac1d4c9317d34fb954bfc15f9684
Marcello Missiroli <prof.missiroli@gmail.com>
2013-04-24 13:28:16 +0200

    [#48444167] Merenda e lista merende

---
+++ El_Merendero/src/el_merendero/Listamerende.java
@@ -0,0 +1,82 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Merendeproject;
+
+/**
+ *
+ * @author b11g12
+ */
+public class Listamerende {
+
+    protected Merenda prodotto;
+    protected Listamerende next;
+
+    public Listamerende(Merenda prodotto) {
+        this.prodotto = null;
+        this.next = new Listamerende();
+        this.next.prodotto = prodotto;
+    }
+
+    public Listamerende() {
+        next=null;
+    }
+
+    public void add(Merenda m) {
+        Listamerende comodo = this;
+        while (comodo.getNext() != null) {
+            comodo = comodo.getNext();
+        }
+        comodo.setNext(new Listamerende());
+        comodo.getNext().setProdotto(m);
+    }
+
+    public Listamerende getNext() {
+        return next;
+    }
+
+    public Merenda getProdotto() {
+        return prodotto;
+    }
+
+    public void setProdotto(Merenda prodotto) {
+        this.prodotto = prodotto;
+    }
+
+    public void setNext(Listamerende next) {
+        this.next = next;
+    }
+
+    public void remove(String s) {
+        Listamerende indice = this;
+        
+        for (indice = this; !indice.getNext().getProdotto().getNome().equals(s); indice = indice.getNext()) {
+            if (indice.getNext() == null) {
+                return;
+            }
+        }
+        indice.setNext(indice.getNext().getNext());
+    }
+
+    public float getTotale() {
+        Listamerende comodo = this.next;
+        float tot = 0f;
+        while (comodo != null) {
+            tot += comodo.getProdotto().getPrezzo();
+            comodo = comodo.getNext();
+        }
+        return tot;
+    }
+
+    @Override
+    public String toString() {
+        String s = "";
+        Listamerende comodo = this.next;
+        while (comodo != null) {
+            s += "Nome = " + comodo.prodotto.getNome() + "  Prezzo = " + comodo.prodotto.getPrezzo() + "\n";
+            comodo = comodo.next;
+        }
+        return s;
+    }
+}
---
+++ El_Merendero/src/el_merendero/Ordine.java
@@ -0,0 +1,70 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Merendeproject;
+
+/**
+ *
+ * @author b11g12
+ */
+public class Ordine {
+
+    private String NomeAcquirente;
+    private float SoldiForniti;
+    private Listamerende MerendeOrdinate;
+    private float resto;
+
+    public float getResto() {
+        return resto;
+    }
+
+    public void setResto() {
+        if (this.ControlloGiustoImporto()) {
+            resto = (float) SoldiForniti - MerendeOrdinate.getTotale();
+        }
+    }
+
+    public boolean ControlloGiustoImporto() {
+        if (SoldiForniti < MerendeOrdinate.getTotale()) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public Ordine(String NomeAcquirente) {
+        this.NomeAcquirente = NomeAcquirente;
+        MerendeOrdinate = new Listamerende();
+    }
+
+    public Listamerende getMerendeOrdinate() {
+        return MerendeOrdinate;
+    }
+
+    public void setMerendeOrdinate(Listamerende MerendeOrdinate) {
+        this.MerendeOrdinate = MerendeOrdinate;
+    }
+
+    public String getNomeAcquirente() {
+        return NomeAcquirente;
+    }
+
+    public void setNomeAcquirente(String NomeAcquirente) {
+        this.NomeAcquirente = NomeAcquirente;
+    }
+
+    public float getSoldiForniti() {
+        return SoldiForniti;
+    }
+
+    public void setSoldiForniti(float SoldiForniti) {
+        this.SoldiForniti = SoldiForniti;
+    }
+
+    @Override
+    public String toString() {
+        return NomeAcquirente + "\n" + MerendeOrdinate;
+    }
+    
+}
